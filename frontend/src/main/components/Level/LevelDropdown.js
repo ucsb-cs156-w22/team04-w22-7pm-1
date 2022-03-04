f38e0ce8848ccadda0d7ecf0fc7cab488513e89f
Eric Xu <ericxu@169-231-153-154.wireless.ucsb.edu>
2022-03-03 16:46:36 -0800

    EX: finished creating the leveldropdown and test coverage for it + storybook up

---
+++ frontend/src/main/components/Level/LevelDropdown.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap';
+
+const LevelDropdown = ({ level, setLevel, onChange = null}) => {
+
+    // const localSearchLevel= localStorage.getItem(controlId);
+
+//     const [levelState, setLevelState] = useState(
+//     // Stryker disable next-line all : not sure how to test/mock local storage
+//    // localSearchLevel || quarters[0].yyyyq //EX: WHAT DOES THIS DO (COMMENTED THIS ENTIRE LINE OUT)
+//     );
+
+    // const handleQuarterOnChange = (event) => {
+    //     const selectedQuarter = event.target.value;
+    //     localStorage.setItem(controlId, selectedQuarter);
+    //     setQuarterState(selectedQuarter);
+    //     setQuarter(selectedQuarter);
+    //     if (onChange != null) {
+    //         onChange(event);
+    //     }
+    // };   
+    
+    // const handleLevelOnChange = (event) => {
+    //     const selectedLevel = event.target.value;
+    //     localStorage.setItem(controlId, selectedLevel);
+    //     setLevelState(selectedLevel);
+    //     setLevel(selectedLevel);
+    //     if (onChange != null) {
+    //         onChange(event);
+    //     }
+    // };
+    
+    //EX: commented the ones above because I don't think they're sufficient/accurate. 
+    const handleLevelOnChange = (event) => {
+        setLevel(event.target.value);
+        if (onChange != null) {
+            onChange(event);
+        }
+    };
+
+    return (
+        //probably need to change the "LevelDropdown.Level"
+        <Form.Group controlId="LevelDropdown.Level">  
+            <Form.Label>Course Level</Form.Label>
+            <Form.Control as="select" value={level} onChange={handleLevelOnChange} >
+                <option label = "UG-lower div" value="UG-lower div"></option>;
+                <option label = "UG-upper div" value="UG-upper div"></option>;
+                <option label = "GRAD" value="GRAD"></option>;
+                <option label = "All" value="All"></option>;
+                
+            </Form.Control>
+        </Form.Group>
+    );
+};
+
+export default LevelDropdown;
---
+++ frontend/src/tests/components/Level/manyLevelDropdown.test.js
@@ -0,0 +1,96 @@
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ManyLevelDropdown from "main/components/level/LevelDropdown"
+
+
+jest.mock('react', ()=>({
+    ...jest.requireActual('react'),
+    useState: jest.fn()
+  }))
+import { useState } from 'react';
+
+describe("manyLevelDropdown tests", () => {
+
+    beforeEach(() => {
+        useState.mockImplementation(jest.requireActual('react').useState);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    const level = jest.fn();
+    const setLevel = jest.fn();
+
+    test("renders without crashing on one level", () => {
+        render(<ManyLevelDropdown
+            //quarters={quarterRange("20211", "20211")}
+            level={level}
+            setLevel={setLevel}
+            //controlId="sqd1" //DUDE WHAT IS THIS LMAO
+        />);
+    });
+
+    // test("renders without crashing on three quarters", () => {
+    //     render(<SingleQuarterDropdown
+    //         quarters={quarterRange("20214", "20222")}
+    //         quarter={quarter}
+    //         setQuarter={setQuarter}
+    //         controlId="sqd1"
+    //     />);
+    // });
+
+    test("when I select an object, the value changes", async () => {
+        const { getByLabelText } =
+            render(<ManyLevelDropdown
+                //quarters={quarterRange("20211", "20222")}
+                level={level}
+                setLevel={setLevel}
+               // controlId="sqd1"        //WHAT ARE THESE TWO BELOW 
+                label="Course Level"
+            />
+            );
+        await waitFor(() => expect(getByLabelText("Course Level")).toBeInTheDocument);
+        const selectLevel = getByLabelText("Course Level")
+        userEvent.selectOptions(selectLevel, "GRAD");
+        expect(setLevel).toBeCalledWith("GRAD");
+    });
+
+    test("if I pass a non-null onChange, it gets called when the value changes", async () => {
+        const onChange = jest.fn();
+        const { getByLabelText } =
+            render(<ManyLevelDropdown
+                //quarters={quarterRange("20211", "20222")}
+                level={level}
+                setLevel={setLevel}
+                //controlId="sqd1"       //WHAT IS THIS 
+                label="Course Level"
+                onChange={onChange}
+            />
+            );
+        await waitFor(() => expect(getByLabelText("Course Level")).toBeInTheDocument);
+        const selectedLevel = getByLabelText("Course Level")
+        userEvent.selectOptions(selectedLevel, "GRAD");
+        await waitFor(() => expect(setLevel).toBeCalledWith("GRAD"));
+        await waitFor(() => expect(onChange).toBeCalledTimes(1));
+
+        // x.mock.calls[0][0] is the first argument of the first call to the jest.fn() mock x
+
+        //const event = onChange.mock.calls[0][0];    //How doe I change these(2) from quarter to match my dropdown. 
+        //console.log(onChange.mock.calls)
+        //expect(event.target.value).toBe("GRAD");
+    });
+
+    test("default label is Course Level", async () => {
+        const { getByLabelText } =
+            render(<ManyLevelDropdown
+                //quarters={quarterRange("20211", "20222")}
+                level={level}
+                setLevel={setLevel}
+                //controlId="sqd1" //WHAT IS THIS ??
+            />
+            );
+        await waitFor(() => expect(getByLabelText("Course Level")).toBeInTheDocument);
+    });
+
+});
