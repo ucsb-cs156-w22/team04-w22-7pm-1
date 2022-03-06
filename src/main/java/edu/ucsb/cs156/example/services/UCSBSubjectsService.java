ffb0653ccd564b0d4c2074bb5dc7d03176eea91f
Rithwik Adicherla <rithwik@ucsb.edu>
2022-03-06 15:59:26 -0800

    should be essentially done but can't figure out how to pass one of the tests due to deserializing issue

--- src/main/java/edu/ucsb/cs156/example/services/UCSBSubjectsService.java
+++ src/main/java/edu/ucsb/cs156/example/services/UCSBSubjectsService.java
@@ -2,18 +2,15 @@ package edu.ucsb.cs156.example.services;
 
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.stereotype.Service;
-
 import edu.ucsb.cs156.example.entities.UCSBSubject;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -21,11 +18,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service("UCSBSubjects")
 public class UCSBSubjectsService {
-        @Autowired
-        private ObjectMapper mapper;
+
+        @Value("${app.ucsb.api.consumer_key}")
+        private String apiKey;
+
+        ObjectMapper mapper = new ObjectMapper();
 
         private final RestTemplate restTemplate;
 
@@ -35,30 +36,36 @@ public class UCSBSubjectsService {
 
         public static final String ENDPOINT = "https://api.ucsb.edu/students/lookups/v1/subjects?includeInactive=false";
 
-        @Value("${app.ucsb.api.consumer_key}")
-        private String apiKey;
-
-        public List<UCSBSubject> get() throws HttpClientErrorException, JsonProcessingException {
-
-                // TODO: Replace with a service that will call the UCSB
-                // developer API at the endpoint
-                // https://api.ucsb.edu/students/lookups/v1/subjects?includeInactive=false
-                // convert the JSON to a List<UCSBSubjects> object
-                // return that object.
-
+        public String getJSON() throws HttpClientErrorException {
                 HttpHeaders headers = new HttpHeaders();
                 headers.setAccept(List.of(MediaType.APPLICATION_JSON));
                 headers.setContentType(MediaType.APPLICATION_JSON);
-                headers.set("ucsb-api-version", "1.0");
                 headers.set("ucsb-api-key", this.apiKey);
+                headers.set("ucsb-api-version", "1.0");
 
-                HttpEntity<String> entity = new HttpEntity<>("body", headers);
+                HttpEntity<String> entity = new HttpEntity<>(headers);
 
                 ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class);
-                String json = re.getBody();
-                // Convert json to list of UCSBSubject objects
-                List<UCSBSubject> result = mapper.readValue(json, new TypeReference<List<UCSBSubject>>() {
-                });
-                return result;
+                return re.getBody();
+                // List<UCSBSubject> fakeResult = mapper.readValue(re.getBody(), new
+                // TypeReference<List<UCSBSubject>>() {
+                // });
         }
+
+        public List<UCSBSubject> get() throws JsonMappingException, JsonProcessingException {
+
+                String json = getJSON();
+                List<UCSBSubject> allSubjects = mapper.readValue(json, new TypeReference<List<UCSBSubject>>() {
+                });
+
+                // List<UCSBSubject> allSubjects = new ArrayList<UCSBSubject>();
+                // try {
+                // UCSBSubject[] subjects = mapper.readValue(json, UCSBSubject[].class);
+                // allSubjects = new ArrayList(Arrays.asList(subjects));
+                // } catch (Exception e) {
+                // e.printStackTrace();
+                // }
+                return allSubjects;
+        }
+
 }
--- src/test/java/edu/ucsb/cs156/example/services/UCSBSubjectsServiceTests.java
+++ src/test/java/edu/ucsb/cs156/example/services/UCSBSubjectsServiceTests.java
@@ -1,47 +1,81 @@
 package edu.ucsb.cs156.example.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import org.junit.jupiter.api.Test;
-import edu.ucsb.cs156.example.ControllerTestCase;
-import edu.ucsb.cs156.example.entities.UCSBSubject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.HttpClientErrorException;
+import edu.ucsb.cs156.example.entities.UCSBSubject;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @RestClientTest(UCSBSubjectsService.class)
-class UCSBSubjectsServiceTests extends ControllerTestCase {
+public class UCSBSubjectsServiceTests {
 
   @Autowired
   private MockRestServiceServer mockRestServiceServer;
 
   @Autowired
-  private UCSBSubjectsService ucsbSubjectsService;
+  private UCSBSubjectsService ucsbSubjectService;
+
+  @Value("${app.ucsb.api.consumer_key}")
+  private String apiKey;
+
+  ObjectMapper mapper = new ObjectMapper();
 
   @Test
-  void get_returns_a_list_of_subjects() throws HttpClientErrorException, JsonProcessingException {
-    String fakeJsonResult = "{ \"fake\" : \"result\" }";
+  public void test_getJSON() {
+
     String expectedURL = UCSBSubjectsService.ENDPOINT;
+    String fakeJsonResult = "{ \"fake\" : \"result\" }";
+
     this.mockRestServiceServer.expect(requestTo(expectedURL))
         .andExpect(header("Accept", MediaType.APPLICATION_JSON.toString()))
         .andExpect(header("Content-Type", MediaType.APPLICATION_JSON.toString()))
+        .andExpect(header("ucsb-api-key", this.apiKey))
+        .andExpect(header("ucsb-api-version", "1.0"))
         .andRespond(withSuccess(fakeJsonResult, MediaType.APPLICATION_JSON));
 
-    List<UCSBSubject> actualResult = ucsbSubjectsService.get();
-    assertEquals(fakeJsonResult, actualResult);
+    assertEquals(fakeJsonResult, ucsbSubjectService.getJSON());
   }
 
+  @Test
+  public void test_get() throws JsonMappingException, JsonProcessingException {
+
+    String expectedURL = UCSBSubjectsService.ENDPOINT;
+    String JsonResult = "{ \"fake\" : \"result\" }";
+
+    this.mockRestServiceServer.expect(requestTo(expectedURL))
+        .andExpect(header("Accept", MediaType.APPLICATION_JSON.toString()))
+        .andExpect(header("Content-Type", MediaType.APPLICATION_JSON.toString()))
+        .andExpect(header("ucsb-api-key", this.apiKey))
+        .andExpect(header("ucsb-api-version", "1.0"))
+        .andRespond(withSuccess(JsonResult, MediaType.APPLICATION_JSON));
+
+    List<UCSBSubject> fakeResult = mapper.readValue(JsonResult, new TypeReference<List<UCSBSubject>>() {
+    });
+
+    // List<UCSBSubject> fakeResult = new ArrayList<>();
+    // try {
+    // UCSBSubject[] subjects = mapper.readValue(JsonResult, UCSBSubject[].class);
+    // fakeResult = new ArrayList(Arrays.asList(subjects));
+
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+
+    assertEquals(fakeResult, ucsbSubjectService.get());
+  }
 }
