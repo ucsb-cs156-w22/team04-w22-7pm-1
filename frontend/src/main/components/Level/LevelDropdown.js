e96ec1dd898f4c5e74bdec7f6a7abe6b749e1d47
Eric Xu <ericxu@169-231-147-248.wireless.ucsb.edu>
2022-03-07 14:26:50 -0800

    EX: Should be 100% good for code review

--- frontend/src/main/components/Level/LevelDropdown.js
+++ frontend/src/main/components/Level/LevelDropdown.js
@@ -3,34 +3,6 @@ import { Form } from 'react-bootstrap';
 
 const ManyLevelDropdown = ({ level, setLevel, onChange = null}) => {
 
-    // const localSearchLevel= localStorage.getItem(controlId);
-
-//     const [levelState, setLevelState] = useState(
-//     // Stryker disable next-line all : not sure how to test/mock local storage
-//    // localSearchLevel || quarters[0].yyyyq //EX: WHAT DOES THIS DO (COMMENTED THIS ENTIRE LINE OUT)
-//     );
-
-    // const handleQuarterOnChange = (event) => {
-    //     const selectedQuarter = event.target.value;
-    //     localStorage.setItem(controlId, selectedQuarter);
-    //     setQuarterState(selectedQuarter);
-    //     setQuarter(selectedQuarter);
-    //     if (onChange != null) {
-    //         onChange(event);
-    //     }
-    // };   
-    
-    // const handleLevelOnChange = (event) => {
-    //     const selectedLevel = event.target.value;
-    //     localStorage.setItem(controlId, selectedLevel);
-    //     setLevelState(selectedLevel);
-    //     setLevel(selectedLevel);
-    //     if (onChange != null) {
-    //         onChange(event);
-    //     }
-    // };
-    
-    //EX: commented the ones above because I don't think they're sufficient/accurate. 
     const handleLevelOnChange = (event) => {
         setLevel(event.target.value);
         if (onChange != null) {
@@ -39,14 +11,14 @@ const ManyLevelDropdown = ({ level, setLevel, onChange = null}) => {
     };
 
     return (
-        //probably need to change the "LevelDropdown.Level"
+
         <Form.Group >  
             <Form.Label>Course Level</Form.Label>
             <Form.Control title = "Course Level" as="select" value={level} onChange={handleLevelOnChange} id="level-dropdown" data-testid="level-dropdown" >
-                <option label = "UG-lower div" value="UG-lower div"></option>;
-                <option label = "UG-upper div" value="UG-upper div"></option>;
-                <option label = "GRAD" value="GRAD"></option>;
-                <option label = "All" value="All"></option>;
+                <option key={"UG-lower div"} label = "UG-lower div" value="UG-lower div"></option>;
+                <option key={"UG-upper div"}label = "UG-upper div" value="UG-upper div"></option>;
+                <option key = {"GRAD"} label = "GRAD" value="GRAD"></option>;
+                <option key = {"All"} label = "All" value="All"></option>;
                 
             </Form.Control>
         </Form.Group>
--- frontend/src/tests/components/Level/LevelDropdown.test.js
+++ frontend/src/tests/components/Level/LevelDropdown.test.js
@@ -1,4 +1,4 @@
-import { getByLabelText, getByTitle, render, waitFor} from "@testing-library/react";
+import { getByLabelText, getByTitle, render, waitFor, fireEvent} from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
@@ -6,33 +6,26 @@ import userEvent from "@testing-library/user-event";
 
 import ManyLevelDropdown from "main/components/Level/LevelDropdown";
 
-
 describe("level dropdown tests", () => {
-
+    const onChange = jest.fn();
+    const setLevel = jest.fn();
     const queryClient = new QueryClient();
-
     test("The Dropdown is able to reassign values and operates", async () => {
 
         const {getByTestId  } = render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <ManyLevelDropdown />
+                    <ManyLevelDropdown onChange={onChange} setLevel = {setLevel}/>
                 </MemoryRouter>
             </QueryClientProvider>
         );
 
         await waitFor(() => expect(getByTestId("level-dropdown")).toBeInTheDocument());
-        //const dropdown = getByTestId("level-dropdown");
 
-        //const selectLevel = getByTitle("Course Level")
         userEvent.selectOptions(getByTestId("level-dropdown"), "GRAD");
-        expect(setLevel).toBeCalledWith("GRAD");
 
-        // const aElement = dropdown.querySelector("All");
-        // expect(aElement).toBeInTheDocument();
-        // aElement?.click();
-        // await waitFor( () => expect(getByTestId("level-dropdown")).toBeInTheDocument() );
-
+        await waitFor(() => expect(setLevel).toBeCalledWith("GRAD"));
+        await waitFor(() => expect(onChange).toBeCalledTimes(1));
     });
 
 
