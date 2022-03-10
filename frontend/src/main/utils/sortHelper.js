170360a1c4a8e2f0753bbb2f6aa202132a8e9ddc
noahzlutz <noahlutz@ucsb.edu>
2022-03-09 17:00:58 -0800

    final mutants fix

--- frontend/src/main/utils/sortHelper.js
+++ frontend/src/main/utils/sortHelper.js
@@ -1,4 +1,4 @@
-
+// Stryker disable next-line all : asc already defualt no change if order = ''
 export function compareValues(key, order = 'asc') {
     return function innerSort(a, b) {
       // Stryker disable next-line all : mutants generate false or result in 0 or undefined
@@ -7,6 +7,7 @@ export function compareValues(key, order = 'asc') {
         return 0;
       }
   
+      // Stryker disable next-line all : if set to false or 'string' removed then result in false and will skip
       const varA = (typeof a[key] === 'string')
         ? a[key].toUpperCase() : a[key];
       const varB = (typeof b[key] === 'string')
