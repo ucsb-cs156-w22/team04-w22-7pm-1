eaee27513f09d3dba95c658a090350804c445c7c
Terry <19tp01@gmail.com>
2022-03-10 15:14:01 -0800

    tp - small table edits

--- frontend/src/main/components/PersonalSchedule/PersonalScheduleTable.js
+++ frontend/src/main/components/PersonalSchedule/PersonalScheduleTable.js
@@ -1,35 +1,31 @@
 import { hasRole } from "main/utils/currentUser";
-import React from "react"
-import OurTable from "../OurTable"
+import React from "react";
+import OurTable from "../OurTable";
 
+export default function PersonalScheduleTable({ schedule, currentUser }) {
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id",
+    },
+    {
+      Header: "name",
+      accessor: "Name",
+    },
+    {
+      Header: "description",
+      accessor: "Description",
+    },
+    {
+      Header: "quarterYYYYQ",
+      accessor: "QuarterYYYYQ",
+    },
+  ];
 
-export default function PersonalScheduleTable ({schedule, currentUser})
-{
-	const columns = [
-		{
-			Header: 'id',
-			accessor: 'id',
-		},
-		{
-			Header: 'name',
-			accessor: 'Name'
-		},
-		{
-			Header: 'description',
-			accessor: 'Description',
-		},
-		{
-			Header: 'quarterYYYYQ',
-			accessor: 'QuarterYYYYQ',
-		}
-	];
+  const memoizedColumns = React.useMemo(() => columns, [columns]);
+  const memoizedSchedule = React.useMemo(() => schedule, [schedule]);
 
-	const memoizedColumns = React.useMemo(() => columns, [columns]);
-	const memoizedSchedule = React.useMemo (() => schedule, [schedule]);
-
-	return <OurTable
-		data={memoizedSchedule}
-		columns = {memoizedColumns}
-		testid = {"PersonalScheduleTbale"}
-		/>;
-};
+  return (
+    <OurTable data={memoizedSchedule} columns={memoizedColumns} testid={"PersonalScheduleTable"} />
+  );
+}
--- frontend/src/main/pages/PersonalSchedule/PersonalScheduleIndexPage.js
+++ frontend/src/main/pages/PersonalSchedule/PersonalScheduleIndexPage.js
@@ -1,24 +1,27 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import React from 'react'
-import PersonalScheduleTable from "main/components/PersonalSchedule/PersonalScheduleTable"
+import React from "react";
+import PersonalScheduleTable from "main/components/PersonalSchedule/PersonalScheduleTable";
 import { useCurrentUser } from "main/utils/currentUser";
 import { useBackend } from "main/utils/useBackend";
 
-
 export default function PersonalScheduleIndexPage() {
-	const currentUser = useCurrentUser;
+  const currentUser = useCurrentUser();
 
-	const {data: schedule, error:_error, status: _status} = 
-	useBackend(["api/PersonalSchedule/all"],
-	{ method: "GET", url: "api/PersonalSchedule/all"},
-		[]
-	);
+  const {
+    data: schedule,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    ["/api/PersonalSchedules/all"],
+    { method: "GET", url: "/api/PersonalSchedules/all" },
+    []
+  );
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>PersonalSchedule</h1>
-		<PersonalScheduleTable schedule={schedule} currentUser ={currentUser} />
+        <PersonalScheduleTable schedule={schedule} currentUser={currentUser} />
       </div>
     </BasicLayout>
-  )
+  );
 }
