0ab0acd3233ccdc46ef43530af0e62c7c20e7153
Phill Conrad <pconrad@cs.ucsb.edu>
2022-03-11 10:03:37 -0800

    Update frontend/src/fixtures/coursesFixtures.js
    Co-authored-by: Andrew Lu <alu@ucsb.edu>

--- frontend/src/fixtures/coursesFixtures.js
+++ frontend/src/fixtures/coursesFixtures.js
@@ -15,7 +15,7 @@ const coursesFixtures = {
             "quarter": "20222",
             "courseId": "ANTH 2 ",
             "title": "INTRO CULT ANTHRO",
-            "description": "The nature of culture: survey of the range of cultural phenomena, including material culture, social organization, religion, and other topics.he nature of culture: survey of the range of cultural phenomena, including material culture, social organization, religion, and other topicshe nature of culture: survey of the range of cultural phenomena, including material culture, social organization, religion, and other topics",
+            "description": "The nature of culture: survey of the range of cultural phenomena, including material culture, social organization, religion, and other topics. The nature of culture: survey of the range of cultural phenomena, including material culture, social organization, religion, and other topics. The nature of culture: survey of the range of cultural phenomena, including material culture, social organization, religion, and other topics",
             "objLevelCode": "U",
             "subjectArea": "ANTH ",
             "unitsFixed": 4,
