0d3b8872115fb9dcfa8c14bb5385ae949fec0b74
Edison Chen <72916325+edisonnchen@users.noreply.github.com>
2022-03-11 12:48:03 -0800

    initial commit

--- frontend/src/main/pages/PersonalSchedule/PersonalScheduleEditPage.js
+++ frontend/src/main/pages/PersonalSchedule/PersonalScheduleEditPage.js
@@ -1,11 +1,77 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import BasicLayout from 'main/layouts/BasicLayout/BasicLayout';
+import { useParams } from 'react-router-dom';
+import PersonalScheduleForm from 'main/components/PersonalSchedule/PersonalScheduleForm';
+import { Navigate } from 'react-router-dom';
+import { useBackend, useBackendMutation } from 'main/utils/useBackend';
+import { toast } from 'react-toastify';
 
-export default function PersonalScheduleEditPage() {
+export default function PersonalSchedulesEditPage() {
+  let { id } = useParams();
+
+  const {
+    data: PersonalSchedule,
+    error: error,
+    status: status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    [`/api/PersonalSchedules?id=${id}`],
+    {
+      // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+      method: 'GET',
+      url: `/api/PersonalSchedules`,
+      params: {
+        id,
+      },
+    }
+  );
+
+  const objectToAxiosPutParams = (PersonalSchedule) => ({
+    url: '/api/PersonalSchedules',
+    method: 'PUT',
+    params: {
+      id: PersonalSchedule.id,
+    },
+    data: {
+      quarterYYYYQ: PersonalSchedule.quarterYYYYQ,
+      name: PersonalSchedule.name,
+      localDateTime: PersonalSchedule.localDateTime,
+    },
+  });
+
+  const onSuccess = (PersonalSchedule) => {
+    toast(
+      `PersonalSchedule Updated - id: ${PersonalSchedule.id} name: ${PersonalSchedule.name}`
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/PersonalSchedules?id=${id}`]
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess) {
+    return <Navigate to="/PersonalSchedules/list" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>PersonalSchedule</h1>
-        <p>This is where the edit page will go</p>
+        <h1>Edit PersonalSchedule</h1>
+        {PersonalSchedule && (
+          <PersonalScheduleForm
+            initialPersonalSchedule={PersonalSchedule}
+            submitAction={onSubmit}
+            buttonLabel="Update"
+          />
+        )}
       </div>
     </BasicLayout>
   );
---
+++ frontend/src/stories/pages/HomePage/PersonalSchedule/PersonalScheduleEditPage.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import PersonalScheduleEditPage from 'main/pages/PersonalSchedule/PersonalScheduleEditPage';
+
+export default {
+  title: 'pages/PersonalSchedule/PersonalScheduleEditPage',
+  component: PersonalScheduleEditPage,
+};
+
+const Template = () => <PersonalScheduleEditPage />;
+
+export const Default = Template.bind({});
