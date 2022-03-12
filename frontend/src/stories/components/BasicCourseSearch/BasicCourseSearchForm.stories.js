a5f29c77739e0704f09258372ff91d1a6c77a323
WAKACHUU <yijiecai@ucsb.edu>
2022-03-11 16:07:30 -0800

    delete useless files and comments

--- frontend/src/stories/components/BasicCourseSearch/BasicCourseSearchForm.stories.js
+++ frontend/src/stories/components/BasicCourseSearch/BasicCourseSearchForm.stories.js
@@ -19,22 +19,3 @@ Default.args = {
   submitText: "Create",
   submitAction: () => { console.log("Submit was clicked"); }
 };
-
-// const initialJSON = {
-//     "pageNumber": 1,
-//     "pageSize": 1,
-//     "total": 0,
-//     "classes": []
-// };
-
-// const setCourseJsonStandin = () => { return initialJSON; };
-// const fetchJSONStandin = () => Promise.resolve();
-
-// const Template = (_args) => {
-//   return(
-//     <BasicCourseSearchForm setCourseJson={setCourseJsonStandin} fetchJSON={fetchJSONStandin} />
-//   )
-// }
-
-// export const Empty = Template.bind({});
-// Empty._args = {};
