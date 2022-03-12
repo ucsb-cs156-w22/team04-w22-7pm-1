4d765a7a1ed13c9efd2602789974b1374dd4a876
Edison Chen <72916325+edisonnchen@users.noreply.github.com>
2022-03-11 17:59:40 -0800

    delete creates a toast

--- frontend/src/main/utils/PersonalScheduleUtils.js
+++ frontend/src/main/utils/PersonalScheduleUtils.js
@@ -1,16 +1,16 @@
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
-import { hasRole } from "main/utils/currentUser";
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import { hasRole } from 'main/utils/currentUser';
 
 export function onDeleteSuccess(message) {
   console.log(message);
-  toast(message);
+  toast(message.message);
 }
 
 export function cellToAxiosParamsDeleteAdmin(cell) {
   return {
-    url: "/api/PersonalSchedules/admin",
-    method: "DELETE",
+    url: '/api/PersonalSchedules/admin',
+    method: 'DELETE',
     params: {
       id: cell.row.values.id,
     },
@@ -19,8 +19,8 @@ export function cellToAxiosParamsDeleteAdmin(cell) {
 
 export function cellToAxiosParamsDeleteUser(cell) {
   return {
-    url: "/api/PersonalSchedules/",
-    method: "DELETE",
+    url: '/api/PersonalSchedules/',
+    method: 'DELETE',
     params: {
       id: cell.row.values.id,
     },
