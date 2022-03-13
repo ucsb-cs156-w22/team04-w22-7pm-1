3646d3df06b31985a573ad1fb016bebb61ea80a3
Edison Chen <72916325+edisonnchen@users.noreply.github.com>
2022-03-12 23:09:54 -0800

    readded create page since its needed to test

--- frontend/src/main/pages/PersonalSchedule/PersonalScheduleCreatePage.js
+++ frontend/src/main/pages/PersonalSchedule/PersonalScheduleCreatePage.js
@@ -1,15 +1,52 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import PersonalScheduleForm from "main/components/PersonalSchedule/PersonalScheduleForm";
+import { Navigate } from "react-router-dom";
+import { useBackendMutation } from "main/utils/useBackend";
+import { useCurrentUser } from "main/utils/currentUser";
+import { toast } from "react-toastify";
 
-export default function PersonalScheduleCreatePage (){
-	return (
-	
-	  <BasicLayout>
-		<div className="pt-2">
-		  <h1>PersonalSchedule</h1>
-		  <p>
-			This is where the create page will go
-		  </p>
-		</div>
-	  </BasicLayout>
-	)
+export default function PersonalScheduleCreatePage() {
+  const currentUser = useCurrentUser();
+
+  const objectToAxiosParams = (personalschedule) => ({
+    url: "/api/PersonalSchedules/post",
+    method: "POST",
+    params: {
+      user: personalschedule.user,
+      name: personalschedule.name,
+      description: personalschedule.description,
+      quarterYYYYQ: personalschedule.quarterYYYYQ,
+    },
+  });
+
+  const onSuccess = (personalschedule) => {
+    toast(`New personalschedule Created - name: ${personalschedule.name}`);
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/PersonalSchedules/all"]
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    mutation.mutate(data);
+  };
+
+  if (isSuccess) {
+    return <Navigate to="/personalschedule/list" />;
+  }
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Create a new personal schedule</h1>
+        <PersonalScheduleForm submitAction={onSubmit} currentUser={currentUser} />
+      </div>
+    </BasicLayout>
+  );
 }
